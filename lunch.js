563d063c096a7c062337534ec132d9ab4b11c6c2
Hari Priya <haripriya.kk2501@gmail.com>
2025-06-14 16:50:24 -0400

    Implement lunch decision logic based on hunger and available time

--- lunch.js
+++ lunch.js
@@ -1,22 +1,16 @@
-/*
- * Modify the contents of the function below, such that:
- *
- * If we're not hungry, we want to tell ourselves to get back to work.
- * Otherwise, we want to pick something up and eat it in the lab when
- * we've got less than 20 minutes or to try a place nearby if we've
- * got between 20 and 30 minutes. If we have any more time than that,
- * we want to remind ourselves that we're in a bootcamp and that we
- * should reconsider how much time we actually have to spare.
- *
- * hungry is a Boolean, representing if you're hungry or not.
- * availableTime is a Number representing the time you have for lunch,
- * in minutes.
- */
-
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
+  if (!hungry) {
+    console.log("Get back to work!");
+  } else {
+    if (availableTime < 20) {
+      console.log("Pick something up and eat it in the lab.");
+    } else if (availableTime >= 20 && availableTime <= 30) {
+      console.log("Try a place nearby.");
+    } else {
+      console.log("You're in a bootcamp! Reconsider how much time you have to spare.");
+    }
+  }
 }
-
 
 /*
  * This is some test runner code that's simply calling our whatToDoForLunch function
@@ -37,3 +31,4 @@ console.log("---");
 
 console.log("I'm hungry and I have 15 minutes for lunch.");
 whatToDoForLunch(true, 15);
+
